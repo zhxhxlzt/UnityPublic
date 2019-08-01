c1fa75639146db66924627ffb27fdff20c38cada
Yiki <2284174996@qq.com>
2019-08-02 01:24:04 +0800

    添加LongPressToggle
    此Toggle用于响应长按事件及触发长按事件时不改变勾选状态

---
+++ Assets/Editor/LongPressToggleEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LongPressToggle))]
+public class LongPressToggleEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var t = (LongPressToggle)target;
+        t.holdTimeLimit = EditorGUILayout.FloatField("holdTimeLimit", t.holdTimeLimit);
+        base.OnInspectorGUI();
+    }
+}
---
+++ Assets/Plugins/GamePublic/Util/LongPressToggle.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class LongPressToggle : Toggle
+{
+    public bool IsClick = false;
+    [Tooltip("按下时间限制")]
+    public float holdTimeLimit = 1; // 按下时间限制，超时则不响应点击，响应长按
+
+    private Coroutine m_countTimeCoroutine;
+
+    public override void OnPointerDown( PointerEventData eventData )
+    {
+        base.OnPointerDown(eventData);
+        IsClick = true;
+        if (m_countTimeCoroutine != null)
+        {
+            StopCoroutine(m_countTimeCoroutine);
+        }
+        m_countTimeCoroutine = StartCoroutine(BeginCountTime());
+    }
+
+    public override void OnPointerUp( PointerEventData eventData )
+    {
+        base.OnPointerUp(eventData);
+        if (m_countTimeCoroutine != null)
+        {
+            StopCoroutine(m_countTimeCoroutine);
+            m_countTimeCoroutine = null;
+        }
+    }
+
+    public override void OnPointerClick( PointerEventData eventData )
+    {
+        if (IsClick)
+        {
+            base.OnPointerClick(eventData);
+        }
+    }
+    
+    IEnumerator BeginCountTime()
+    {
+        yield return new WaitForSeconds(holdTimeLimit);
+        IsClick = false;
+        OnLongPress();
+    }
+
+    protected virtual void OnLongPress()
+    {
+        Debug.Log("Long ========= Press");
+    }
+
+}
